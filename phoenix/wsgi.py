9d8f6fa8ea11d7eafe6fabed39b3e5c9bd32690e
Manikanta-u94 <46936521+Manikanta-u94@users.noreply.github.com>
2019-03-04 15:18:39 +0530

    Update wsgi.py

--- phoenix/wsgi.py
+++ phoenix/wsgi.py
@@ -1,41 +1,16 @@
 """
-WSGI config for phoenix project.
+WSGI config for myshop project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-# import os
+import os
 
-# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "phoenix.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'phoenix.settings')
 
-# application = get_wsgi_application()
-
-import myapp.wsgi
-SCRIPT_NAME = '/home/phoenix/hercules'
-
-class PassengerPathInfoFix(object):
-    """
-    Sets PATH_INFO from REQUEST_URI because Passenger doesn't provide it.
-    """
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        from urllib.parse import unquote
-        environ['SCRIPT_NAME'] = SCRIPT_NAME
-
-        request_uri = unquote(environ['REQUEST_URI'])
-        script_name = unquote(environ.get('SCRIPT_NAME', ''))
-        offset = request_uri.startswith(script_name) and len(environ['SCRIPT_NAME']) or 0
-        environ['PATH_INFO'] = request_uri[offset:].split('?', 1)[0]
-        return self.app(environ, start_response)
-
-application = hercules.wsgi.application
-application = PassengerPathInfoFix(application)
-
-
+application = get_wsgi_application()
